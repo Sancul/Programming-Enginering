f1b9edc27ccf0b03ee4707b0deb3fe31f8084208
gen-wandering <125150436+gen-wandering@users.noreply.github.com>
2023-03-06 18:57:10 +0300

    Code

---
+++ entities/Rating.java
@@ -0,0 +1,8 @@
+package com.tests.assignments.teamwork.entities;
+
+public record Rating(double kp,
+                     double imdb,
+                     double filmCritics,
+                     double russianFilmCritics,
+                     double await) {
+}
